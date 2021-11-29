c6741f0e7aa893730ba4810e0aab0f5215e4fde7
Chika Hope <45790943+swiftDev1@users.noreply.github.com>
2021-11-29 13:04:21 +0000

    step 2 imporved

--- main.py
+++ main.py
@@ -12,21 +12,22 @@ print(f'Pssst, the solution is {chosen_word}.')
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 
 display = []
-# for letter in chosen_word:
-#   display.append('_')
+word_length = len(chosen_word)
+for index in range(word_length):
+  display.append('_')
 
-# print(display)
+print(display)
 
 guess = input("Guess a letter: ").lower()
 
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for letter in chosen_word:
+for index in range(word_length):
+    letter = chosen_word[index]
     if letter == guess:
-      display.append(letter)
-    else:
-        display.append('_')
+      display[index] = letter
+
 
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
